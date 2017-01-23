b40b699d883251f66d27a16f1bd5539e4940a2d8
Aurora Leeson <aleeson@g.hmc.edu>
2017-01-22 16:37:10 -0800

    Fixed code and removed unnecessary file

--- impulse_response.m
+++ impulse_response.m
@@ -1,15 +1,19 @@
 function [ h,t ] = impulse_response( x, y, fs )
+
 %impulse_response returns the impulse response of the system given the
 %system input, x, and system output, y.
 %   inputs: x, measured input signal
 %           y, measured output signal
 %           fs, sampling frequency of measurements
 %   outputs:    h, calculated impulse response
+%		t, corresponding time values || Aurora Leeson 1/22/17
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
 H = X./Y;   % the calculated frequency response
-[h,t] = tdomain(H,fs);
+[h,t] = tdomain(H,f); % Aurora Leeson 1/22/17
+
+plot(h,t);
 
 end
 
